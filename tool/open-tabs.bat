942b0c1447cf05aed6169d0e9874561f1847f2ca
nakayama-kazuki <kanakaya@yahoo-corp.jp>
2021-07-04 09:07:02 +0900

    check length

--- tool/open-tabs.bat
+++ tool/open-tabs.bat
@@ -1,17 +1,19 @@
-@powershell "$PATH=\"%~f0\"; $EXEC=[scriptblock]::create((Get-Content $PATH | Where-Object {$_.readcount -gt 1}) -join\"`n\"); & $EXEC" & goto:eof
+@powershell "$THISFILE=\"%~f0\"; $PSCODE=[scriptblock]::create((Get-Content $THISFILE | Where-Object {$_.readcount -gt 1}) -join \"`n\"); & $PSCODE" & goto:eof
 
-$src = Get-Clipboard -Format Text;
+$copiedText = Get-Clipboard -Format Text;
 
 $urls = @();
-foreach ($line in $src) {
-	if ($line.IndexOf('https:') -eq 0)  {
+foreach ($line in $copiedText) {
+	if ($line.IndexOf("https:") -eq 0)  {
 		$urls += $line;
 		Write-Host "open $line";
 	}
 }
 
-$url = $urls -join " ";
-$option = "--incognito --new-window $url";
-$chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
-
-Start-Process -FilePath $chrome -ArgumentList $option;
+if ($urls.Length -gt 0) {
+	$url = $urls -join " ";
+	$chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+	Start-Process -FilePath $chrome -ArgumentList "--incognito --new-window $url";
+} else {
+	[System.Windows.Forms.MessageBox]::Show("404 URL Not Found");
+}
