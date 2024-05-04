ccf3669177a4e642159df188a2bd7b40761d0f91
nakayama-kazuki <pj.corridor@gmail.com>
2024-05-04 09:41:28 +0900

    fixed

--- tool/browserscreen2image.bat
+++ tool/browserscreen2image.bat
@@ -26,42 +26,35 @@ if ($OPTION.ContainsKey('r')) {
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-add-type -AssemblyName microsoft.VisualBasic
-
-Add-Type @"
-	using System;
-	using System.Runtime.InteropServices;
-	public struct WINDOWS_API_RECT {
-		public int L;
-		public int T;
-		public int R;
-		public int B;
-	}
-	public class WINDOWS_API {
-		[DllImport("user32.dll")]
-		public static extern bool IsIconic(IntPtr hWnd);
-		[DllImport("user32.dll")]
-		public static extern bool GetWindowRect(IntPtr hWnd, out WINDOWS_API_RECT lpRect);
-		[DllImport("user32.dll")]
-		public static extern bool GetClientRect(IntPtr hWnd, out WINDOWS_API_RECT lpRect);
-		[DllImport("user32.dll")]
-		public static extern bool ClientToScreen(IntPtr hWnd, ref WINDOWS_API_RECT lpPoint);
-	}
-"@
+Add-type -AssemblyName microsoft.VisualBasic
+Add-Type -AssemblyName UIAutomationClient
 
 Set-Variable -Name FILEPATH -Value ([System.Environment]::GetFolderPath('Desktop')) -Option Constant
 Set-Variable -Name IMAGEMAGICK -Value 'C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe' -Option Constant
 
 <#
-	0. settings
+	0. settings : BWIN_MARGIN_T is optimized for chrome
 #>
 
 Set-Variable -Name PNG_PREFIX -Value ($TARGETAPP + '-screen') -Option Constant
-Set-Variable -Name BROWSERUI_HEIGHT -Value 120 -Option Constant
+Set-Variable -Name BWIN_MARGIN_L -Value 8 -Option Constant
+Set-Variable -Name BWIN_MARGIN_T -Value 121 -Option Constant
+Set-Variable -Name BWIN_MARGIN_R -Value 8 -Option Constant
+Set-Variable -Name BWIN_MARGIN_B -Value 8 -Option Constant
 
 <#
-	1. find $TARGETAPP window
+	1. get rectangle of $TARGETAPP window
 #>
+
+function isIconic($in_e) {
+    $supportedPatterns = $in_e.GetSupportedPatterns()
+    $isSupported = $supportedPatterns.Id -contains [System.Windows.Automation.WindowPattern]::Pattern.Id
+	if ($isSupported) {
+		$pattern = $in_e.GetCurrentPattern([System.Windows.Automation.WindowPattern]::Pattern)
+		return $pattern.Current.WindowVisualState -eq [System.Windows.Automation.WindowVisualState]::Minimized
+	}
+	return $false
+}
 
 function getProcess($in_app) {
 	$processList = Get-Process -Name $in_app -ErrorAction SilentlyContinue
@@ -73,23 +66,41 @@ function getProcess($in_app) {
 		if ($process.MainWindowHandle -eq 0)  {
 			continue
 		}
-		if (![WINDOWS_API]::IsIconic($process.MainWindowHandle)) {
+		$autoElem = [System.Windows.Automation.AutomationElement]::FromHandle($process.MainWindowHandle)
+		if (isIconic $autoElem) {
+			continue
+		}
+		if ($autoElem.Current.Name) {
 			[Microsoft.VisualBasic.Interaction]::AppActivate($process.ID)
 			return $process
 		}
 	}
-	Write-Host "error : $($in_app) is minimized"
+	Write-Host "error : $($in_app) is not normal state"
 	return $null
 }
 
-$process = getProcess $TARGETAPP
+function getDrawingRect($in_hWnd) {
+	$autoElem = [System.Windows.Automation.AutomationElement]::FromHandle($in_hWnd)
+	$r = $autoElem.Current.BoundingRectangle
+	# Write-Host "X:$($r.X), Y:$($r.Y), W:$($r.Width), H:$($r.Height)"
+	return New-Object System.Drawing.Rectangle(
+		($r.X + $BWIN_MARGIN_L),
+		($r.Y + $BWIN_MARGIN_T),
+		($r.Width - $BWIN_MARGIN_L - $BWIN_MARGIN_R),
+		($r.Height - $BWIN_MARGIN_T - $BWIN_MARGIN_B)
+	)
+}
 
-if (!$process) {
+$targetProcess = getProcess $TARGETAPP
+
+if ($targetProcess) {
+	$targetRect = getDrawingRect $targetProcess.MainWindowHandle
+} else {
 	exit
 }
 
 <#
-	2. copy whole primary screen to $screen
+	2. copy whole primary screen to $screenBmp
 #>
 
 $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
@@ -104,23 +115,18 @@ $graphics.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bound
 	3. crop $screen of '2' using rect of '1'
 #>
 
-[WINDOWS_API_RECT] $windowRect = New-Object WINDOWS_API_RECT
-$res = [WINDOWS_API]::GetWindowRect($process.MainWindowHandle, [ref] $windowRect)
-# Write-Host "L:$($windowRect.L), T:$($windowRect.T), R:$($windowRect.R), B:$($windowRect.B)"
+$screenRect = New-Object System.Drawing.Rectangle(0, 0, $bounds.Width, $bounds.Height)
 
-[WINDOWS_API_RECT] $clientRect = New-Object WINDOWS_API_RECT
-$res = [WINDOWS_API]::GetClientRect($process.MainWindowHandle, [ref] $clientRect)
-# Write-Host "L:$($clientRect.L), T:$($clientRect.T), R:$($clientRect.R), B:$($clientRect.B)"
+if ($screenRect.Contains($targetRect)) {
+	$targetBmp = $screenBmp.Clone($targetRect, $screenBmp.PixelFormat)
+} else {
+	Write-Host 'error : rect of screen does not contain rect of target'
+	exit
+}
 
-$marginX = $windowRect.R - $windowRect.L - $clientRect.R
-$marginY = $windowRect.B - $windowRect.T - $clientRect.B
-
-$targetRect = New-Object System.Drawing.Rectangle(
-	($windowRect.L + $marginX / 2),
-	($windowRect.T + $BROWSERUI_HEIGHT),
-	($windowRect.R - $windowRect.L - $marginX),
-	($windowRect.B - $windowRect.T - $marginY - $BROWSERUI_HEIGHT)
-)
+<#
+	4. save bitmap of '3' and exec $IMAGEMAGICK
+#>
 
 function getSavePath() {
 	$index = 0
@@ -132,7 +138,6 @@ function getSavePath() {
 }
 
 $savePath = getSavePath
-$targetBmp = $screenBmp.Clone($targetRect, $screenBmp.PixelFormat)
 $targetBmp.Save($savePath, [System.Drawing.Imaging.ImageFormat]::Png)
 Start-Process $IMAGEMAGICK -ArgumentList $savePath, '-colors 256', '-depth 8', $RESIZE, $savePath -NoNewWindow -Wait
 
